1d550ab109ba847d0e7b0ed002c234f025b68309
Sammybams <samuelbamgbola@gmail.com>
2023-04-26 19:27:55 +0100

    Added columns and date input for column1.

--- pages/2_🌍_Forecasting_Toolkit.py
+++ pages/2_🌍_Forecasting_Toolkit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
+import datetime
 from urllib.error import URLError
 
 st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
@@ -12,10 +13,21 @@ st.subheader(
     """Forecast the occurence in a location based on observed disorder type and date."""
 )
 
-@st.experimental_memo
-def from_data_file(filename):
-    url = (
-        "http://raw.githubusercontent.com/streamlit/"
-        "example-data/master/hello/v1/%s" % filename
-    )
-    return pd.read_json(url)
+# @st.experimental_memo
+# def from_data_file(filename):
+#     url = (
+#         "http://raw.githubusercontent.com/streamlit/"
+#         "example-data/master/hello/v1/%s" % filename
+#     )
+#     return pd.read_json(url)
+
+
+col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+
+
+with col1:
+    d = st.date_input(
+        "Expected Date",
+        datetime.datetime.today())
+    
