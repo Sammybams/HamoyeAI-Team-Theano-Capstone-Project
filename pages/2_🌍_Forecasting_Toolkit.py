14c8a9536d9879d197b1a84bf88bf12e372c5a53
Sammybams <samuelbamgbola@gmail.com>
2023-05-01 12:09:12 +0100

    Display test dataframe to confirm order of columns.

--- pages/2_🌍_Forecasting_Toolkit.py
+++ pages/2_🌍_Forecasting_Toolkit.py
@@ -166,5 +166,6 @@ test['year'] = test.EVENT_DATE.dt.year
 test = pd.get_dummies(test, columns = ['DISORDER_TYPE', 'REGION', 'COUNTRY', 'EVENT_TYPE', 'SUB_EVENT_TYPE', 'INTER1', 'INTER2'])
 test.drop(['ADMIN1', 'LOCATION', 'ACTOR1', 'EVENT_DATE'], axis=1, inplace=True)
 
+st.dataframe(test)
 if st.button("Run"):
     st.header("Coming soon")
