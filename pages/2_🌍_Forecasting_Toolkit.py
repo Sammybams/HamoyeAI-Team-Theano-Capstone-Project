ee6f7d6d790e6bec90d55c19bb135d505668e71d
Sammybams <samuelbamgbola@gmail.com>
2023-05-05 19:07:19 +0100

    Restored to previous.

--- pages/2_🌍_Forecasting_Toolkit.py
+++ pages/2_🌍_Forecasting_Toolkit.py
@@ -71,15 +71,15 @@ mapping_binned_reverse = {
     6: "0"
 }
 
-correct_order = {
-    "101 TO 500": 5,
-    "11 TO 50": 3,
-    "1": 0,
-    "2 TO 10": 2,
-    "501 TO 1350" :6,
-    "51 TO 100": 4,
-    "0": 0 
-}
+# correct_order = {
+#     "101 TO 500": 5,
+#     "11 TO 50": 3,
+#     "1": 0,
+#     "2 TO 10": 2,
+#     "501 TO 1350" :6,
+#     "51 TO 100": 4,
+#     "0": 0 
+# }
 
 date = st.date_input(
     "Choose expected date",
@@ -227,15 +227,17 @@ if st.button("Run"):
                       'LOCATION', 'EVENT TYPE', 'SUB EVENT TYPE', 'MAIN ACTOR INVOLVED',
                       'MAIN ACTOR TYPE', 'SUBSIDIARY ACTOR TYPE', 'PREDICTED FATALITIES']
     
-    record['CORRECT ORDER'] = record['PREDICTED FATALITIES'].map(correct_order)
-    record = record.sort_values(by=['CORRECT ORDER'], ascending=False)
+    # record['CORRECT ORDER'] = record['PREDICTED FATALITIES'].map(correct_order)
+    record = record.sort_values(by=['PREDICTED FATALITIES'], ascending=False)
 
-    st.write(record)
+    # record = record.sort_values(by=['CORRECT ORDER'], ascending=False)
+
+    # st.write(record)
 
     record.reset_index(drop=True, inplace=True)
     record.index = record.index + 1
 
-    record.drop(['CORRECT ORDER'], axis=1, inplace=True)
+    # record.drop(['CORRECT ORDER'], axis=1, inplace=True)
 
     top_2 = record.head(2)
     # Reset index and add 1 to index to start at 1
